5ab027785c187b9d66389d6b96bc4a8df664325f
zesciati <zakyabiyyu2003@gmail.com>
2023-08-19 14:54:27 +0800

    Menambahkan 1 line di index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello world !</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi architecto hic dignissimos accusamus officiis quia praesentium corrupti autem, voluptatem dolorem nostrum debitis odit atque. Nobis beatae dolores quibusdam nisi deserunt?</p>
+    <p>The End</p>
 </body>
 </html>
